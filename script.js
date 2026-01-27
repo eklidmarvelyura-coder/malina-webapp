7faa62ecfd816a7aa6ea76347dba1b8837ef117a
eklidmarvelyura-coder <eklidmarvelyura@gmail.com>
2026-01-27 22:25:44 +0700

    Update feedback alert messages in sendFeedback function

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function sendFeedback() {
     const text = document.getElementById('feedbackText').value;
 
     if (text.trim() === '') {
-        alert('Пожалуйста, напишите сообщение');
+        alert('Пожалуйста, не оставляйте поле пустым :)');
         return;
     }
 
-    alert('Спасибо за обратную связь!');
+    alert('Спасибо за обратную связь! Рады стараться для Вас!');
     document.getElementById('feedbackText').value = '';
 }
+
